d1607caac5d2a59de10f00f78179d88595fdd429
OlehMoruhyi <olehmoruhyi@gmail.com>
2023-03-30 16:47:06 +0300

    Make migrations and start work this home view

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Job)
+admin.site.register(Category)
+admin.site.register(Seeker)
+admin.site.register(Employer)
+admin.site.register(Offer)
+admin.site.register(OfferResponse)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from django.views.generic import View
+from .models import User, Offer
 
 from .forms import NewUserForm
 
@@ -21,8 +22,11 @@ def register_request(request):
 
 class HomeView(View):
     template_name = 'index.html'
+
     def get(self, request):
-        return render(request, 'main/index.html')
+        recent = Offer.objects.all()
+        spotlight = Offer.objects.all()
+        return render(request, 'main/index.html', {'recent': recent, 'spotlight': spotlight})
 
 
 class ProfileView(View):  # Serhii
